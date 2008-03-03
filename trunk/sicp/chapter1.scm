a510e6fd8a0f244485f7fce5002ccd5f51a4aa05
alex.rudnick <alex.rudnick@8ef98eb1-aa40-0410-ae41-5dfa0a5cf510>
2008-03-03 07:10:47 +0000

    Fix to problem 1.3, kept on trucking a bit, added the source from the book for reference.

--- trunk/sicp/chapter1.scm
+++ trunk/sicp/chapter1.scm
@@ -2,19 +2,19 @@
 ;; Chapter 1
 ;; Lindsey Kuper and Alex Rudnick
 
-;; 1.1 (alexr)
+;; 1.1
 ;; What is the result printed by the interpreter in response to each
 ;; expression? ...
 
 
-;; 1.2 (alexr)
+;; 1.2
 ;; Translate the following expression into prefix form.
 
 (define (one-point-two)
   (/ (+ 5 4 (- 2 (- 3 (+ 6 4/5))))
      (* 3 (- 6 2) (- 2 7))))
 
-;; 1.3 (alexr)
+;; 1.3
 ;; Define a procedure that takes three numbers as arguments and returns
 ;; the sum of squares of the two larger numbers.
 
@@ -26,7 +26,7 @@
 (define (sum-of-squares-of-largest-two a b c)
   (cond
    ;; a is the smallest.
-   ((and (< a b) (a c))
+   ((and (< a b) (< a c))
     (print 12)
     (sum-of-squares b c))
 
@@ -37,3 +37,53 @@
    ;; otherwise c is the smallest.
    (#t
     (sum-of-squares a b))))
+
+;; 1.4
+;; Observe that our model of evaluation allows for combinations whose operators
+;; are compound expressions. Use this observation to describe the behavior of
+;; the following procedure:
+(define (a-plus-abs-b a b)
+  ((if (> b 0) + -) a b))
+
+;; The body of the function takes the form:
+;; ( (expression that evaluates to a function) a b)
+;; ... and the value of the expression is either + or -, such that we always add
+;; the absolute value of b to a.
+
+;; 1.5
+;; Ben Bitdiddle defines the following procedures...
+
+(define (p) (p))
+(define (test x y)
+  (if (= x 0)
+      0
+      y))
+
+;; He then evaluates the expression (test 0 (p)).
+;; What behavior will Ben observe with an interpreter that uses
+;; applicative-order evaluation? What behavior will he observe with an
+;; interpreter that uses normal-order evaluation? ...
+
+;; alexr: In the applicative-order case, the interpreter should freeze up and
+;; not return any value at all; in order to evaluate the expression (test 0
+;; (p)), we would find the value of each part of the expression first --
+;; yielding, in order, a procedure, 0... and a tail-recursive infinite loop.
+
+;; The normal-order case (** XXX: is this the same as lazy evaluation, like in
+;; Haskell? Or is it like thunks and call-by-name? Or are these the same thing?
+;; **) would expand out the expression to be (if (= 0 0) 0 (p)), and evaluate
+;; out to 0, never jumping into the infinite tail recursion.
+
+;; 1.6
+;; Alyssa P. Hacker doesn't see why /if/ needs to be provided as a special
+;; form...
+(define (new-if predicate then-clause else-clause)
+  (cond (predicate then-clause)
+	(else else-clause)))
+
+;; Delighted, Alyssa uses new-if to rewrite the square root program:
+(define (sqrt-iter guess x)
+  (new-if (good-enough? guess x)
+	  guess
+	  (sqrt-iter (improve guess x)
+		     x)))
