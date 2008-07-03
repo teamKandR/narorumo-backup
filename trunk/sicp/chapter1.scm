8262f8b535508ffa501ee2b045f52ded649cbdbb
lindseykuper <lindseykuper@8ef98eb1-aa40-0410-ae41-5dfa0a5cf510>
2008-07-03 16:43:11 +0000

    Some of 1.31.

--- trunk/sicp/chapter1.scm
+++ trunk/sicp/chapter1.scm
@@ -1653,6 +1653,34 @@
 
 ;; seems to work!
 
+;;;; 1.31a 
+;; ...Write an analogous procedure called /product/ that returns the
+;; product of the values of a function at points over a given range.
+;; Show how to define /factorial/ in terms of /product/.  Also use
+;; /product/ to compute approximations to pi using the formula
+;;
+;; pi   2 * 4 * 4 * 6 * 6 * 8 ...
+;; -- = -------------------------
+;;  4   3 * 3 * 5 * 5 * 5 * 7 ...
+
+(define (product term a next b)
+  (if (> a b)
+      1
+      (* (term a)
+         (product term (next a) next b))))
+
+(define (factorial num) (product * 1 inc num))
+           
+;;;; 1.31b
+;; ...write one that generates an iterative process.
+
+(define (product-iter term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (* (term a) result))))
+  (iter a 1))
+
 ;;;; 1.34
 ;; Suppose we define the procedure
 
