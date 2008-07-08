f8d78342eb8afc690d6ebc8b26fa0ad62fef9ff0
lindseykuper <lindseykuper@8ef98eb1-aa40-0410-ae41-5dfa0a5cf510>
2008-07-08 15:46:25 +0000

    1.35!

--- sicp/chapter1.scm
+++ sicp/chapter1.scm
@@ -1946,5 +1946,32 @@
 ;; So, (f fib) would return 1, but (f xor) would return an error because the
 ;; /xor/ procedure expects 2 arguments.)
 
+;;;; 1.35
+;; Show that the golden ratio phi is a fixed point of the transformation
+;; x --> 1 + 1/x...
 
+;; A fixed point of x --> 1 + 1/x is a point at which 
+;; x = 1 + 1/x, which is equivalent to x^2 = x + 1.  The golden ratio is the
+;; number phi which satisfies the equation phi^2 = phi + 1.  Therefore a fixed
+;; point of x --> 1 + 1/x is phi, the golden ratio.
 
+;; ...and use this fact to compute phi by means of the /fixed-point/ procedure.
+
+(define tolerance 0.00001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+(define golden-ratio-using-fixed-point
+  (fixed-point (lambda (x) (+ 1 (/ 1 x)))
+               1.0))
+
+;> golden-ratio-using-fixed-point
+;1.6180327868852458
