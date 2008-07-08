69e7887d819b825f3f62b1bf16e6a1c22a562ea0
lindseykuper <lindseykuper@8ef98eb1-aa40-0410-ae41-5dfa0a5cf510>
2008-07-08 17:15:06 +0000

    1.36! x

--- sicp/chapter1.scm
+++ sicp/chapter1.scm
@@ -1975,3 +1975,83 @@
 
 ;> golden-ratio-using-fixed-point
 ;1.6180327868852458
+
+;;;; 1.36
+
+(define (verbose-fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (begin
+        (display guess)
+        (newline)
+        (if (close-enough? guess next)
+            next
+            (try next)))))
+  (try first-guess))
+
+(define exercise-1-36-without-average-damping
+  (lambda ()
+    (verbose-fixed-point (lambda (x) (/ (log 1000) (log x)))
+                 4.0)))
+
+;> (exercise-1-36-without-average-damping)
+;4.0
+;4.9828921423310435
+;4.301189432497896
+;4.734933901055578
+;4.442378437719526
+;4.632377941509958
+;4.505830646780212
+;4.588735606875766
+;4.533824356566501
+;4.56993352418142
+;4.546075272637246
+;4.561789745175654
+;4.55141783665413
+;4.5582542120702625
+;4.553744140202578
+;4.556717747893265
+;4.554756404545319
+;4.5560497413912975
+;4.5551967522618035
+;4.555759257615811
+;4.555388284933278
+;4.555632929754932
+;4.555471588998784
+;4.555577989320218
+;4.555507819903776
+;4.555554095154945
+;4.555523577416557
+;4.555543703263474
+;4.555530430629037
+;4.555539183677709
+
+;; 30 steps without average damping.
+
+;; For the version with average damping, we take the original function, add /x/
+;; to both sides and divide by 2.
+
+;;  x -->           log(1000)/log(x)
+;; 2x -->       x + log(1000)/log(x)
+;;  x --> (1/2)(x + log(1000)/log(x))
+
+(define exercise-1-36-with-average-damping
+  (lambda ()
+    (verbose-fixed-point (lambda (x) (average x (/ (log 1000) (log x))))
+                 4.0)))
+
+;> (exercise-1-36-with-average-damping)
+;4.0
+;4.491446071165521
+;4.544974650975552
+;4.553746974742814
+;4.555231425802502
+;4.555483906560562
+;4.5555268862194875
+;4.5555342036887705
+
+;; Only 8 steps.  (That is, 8 times through /fixed-point/, but not necessarily
+;; less processing.)
+
