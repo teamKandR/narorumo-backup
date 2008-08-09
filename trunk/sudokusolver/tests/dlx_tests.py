5f9e0d89d48c64face54b88e5fc25e705403f139
alex.rudnick <alex.rudnick@8ef98eb1-aa40-0410-ae41-5dfa0a5cf510>
2008-08-09 05:34:42 +0000

    Blow away some old code and get ready to make search happen!

--- trunk/sudokusolver/tests/dlx_tests.py
+++ trunk/sudokusolver/tests/dlx_tests.py
@@ -1,6 +1,29 @@
 from unittest import TestCase
 
-class DlxTests(TestCase):
+from sudokudlx.sparsematrix import SparseMatrix
+from sudokudlx.dlx import DLX
+from copy import deepcopy
 
-  def testTestCase(self):
+rows1 = [[0, 0, 1, 0, 1, 1, 0],
+         [1, 0, 0, 1, 0, 0, 1],
+         [0, 1, 1, 0, 0, 1, 0],
+         [1, 0, 0, 1, 0, 0, 0],
+         [0, 1, 0, 0, 0, 0, 1],
+         [0, 0, 0, 1, 1, 0, 1]]
+
+## Only columns 3 and 5 have 1s. This is an unsolvable cover problem.
+rows2 = [[0, 0, 0, 0, 0, 1, 0],
+         [0, 0, 0, 1, 0, 0, 0],
+         [0, 0, 0, 0, 0, 1, 0],
+         [0, 0, 0, 1, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 1, 0, 0, 0]]
+
+dlx = DLX(rows1)
+
+class DlxTests(TestCase):
+  def testSearch(self):
+    assert True
+
+  def testChooseColumn(self):
     assert True
