b65c9fa4b3f588f7a1fc9b6cbee808e3a5f10e94
lindseykuper <lindseykuper@8ef98eb1-aa40-0410-ae41-5dfa0a5cf510>
2009-01-09 00:05:40 +0000

    A little more concise now that I know about dict.setdefault() and sorted().

--- trunk/perturbation/lindsey_perturbation.py
+++ trunk/perturbation/lindsey_perturbation.py
@@ -12,26 +12,17 @@ total_perturbations = 0
 
 output = open('perturbations.txt', 'w')
 
-def sort_str(str):
-    chars = []
-    for char in str:
-        chars.append(char)
-    chars.sort()
-    sorted_str = string.join(chars)
-    return sorted_str
-
 for word in open('/usr/share/dict/words'):
     word = word.strip()
     orig_words.append(word)
-    sorted_word = sort_str(word)
-    if sorted_word not in sorted_to_unsorted:
-        sorted_to_unsorted[sorted_word] = [];
+    sorted_word = string.join(sorted(word))
+    sorted_to_unsorted.setdefault(sorted_word, [])
     sorted_to_unsorted[sorted_word].append(word);
     
 for word in orig_words:
     if re.match('.*m.*', word):
         rb_word = re.sub('m', 'rb', word, 1) # only replace one m!
-        sorted_rb_word = sort_str(rb_word)
+        sorted_rb_word = string.join(sorted(rb_word))
         if sorted_rb_word in sorted_to_unsorted:
             print >>output, word, "perturbs to:"
             for word in sorted_to_unsorted[sorted_rb_word]:
