3a261cd318ab409052ff9646840680a53564b95d
lindseykuper <lindseykuper@8ef98eb1-aa40-0410-ae41-5dfa0a5cf510>
2009-01-15 23:28:12 +0000

    Could've done this one with reduce, too.

--- projecteuler/1.scm
+++ projecteuler/1.scm
@@ -11,3 +11,29 @@
       (kernel (- n 1) 0))))
 
 (problem-1 1000)
+
+;; Could've also been done with reduce:
+
+(define reduce
+  (lambda (f ls)
+    (letrec ((reduce
+              (lambda (f ls acc)
+                (cond
+                  [(null? ls) acc]
+                  [(null? (cdr ls))  (f (car ls) acc)]
+                  [else (reduce f (cddr ls) (+ acc (f (car ls) (cadr ls))))]))))
+      (reduce f ls 0))))
+
+(define problem-1-with-reduce
+  (lambda (n)
+    (display
+     (reduce + (filter (lambda (x)
+                         (or (= (remainder x 5) 0)
+                             (= (remainder x 3) 0)))
+                       (letrec ((range (lambda (n)
+                                         (cond
+                                           [(zero? n) '()]
+                                           [else (cons n (range (- n 1)))]))))
+                         (range (- n 1))))))))
+
+(problem-1-with-reduce 1000)
