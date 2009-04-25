858ddb486fc05a54aa56c49ab0c6afab4b0c1a39
alex.rudnick <alex.rudnick@8ef98eb1-aa40-0410-ae41-5dfa0a5cf510>
2009-04-25 08:01:38 +0000

    Updated plats to handle the (very common) case of no Accept-Language header being sent. Also made a slight refactor and made the front page be static, since we weren't doing any templating anyway.

--- plats/plats.py
+++ plats/plats.py
@@ -1,3 +1,11 @@
+def get_accept_language(request):
+  if "accept-language" in request.headers:
+    out = request.headers["accept-language"]
+  else:
+    out = ""
+
+  return out
+
 def pick_locale(request):
   """Given a WebOb Request object, presumably with a query parameter called
   "available", return the best locale setting."""
@@ -5,9 +13,8 @@ def pick_locale(request):
   available_parm = request.get("available")
   if not available_parm:
     available_parm = "none"
-
   available = available_parm.split(",")
-  preferred = ordered_locales(request.headers['accept-language'])
+  preferred = ordered_locales(request)
 
   best = pick(available, preferred)
   return best
@@ -21,9 +28,11 @@ def pick(available, preferred):
       return lang
   return available[0]
 
-def ordered_locales(accept_language):
-  """Take the Accept-Language header string and produce an ordered list of
+def ordered_locales(request):
+  """Take a request and produce an ordered list of
   language strings, from most preferred to least."""
+
+  accept_language = get_accept_language(request)
   langs = accept_language.split(",")
   pairs = map( lambda(lang): lang.split(";"), langs)
 
