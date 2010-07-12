373d2e57b1bfe1362efd46ed494d64aa60d8a4de
alex.rudnick <alex.rudnick@8ef98eb1-aa40-0410-ae41-5dfa0a5cf510>
2010-07-12 02:23:46 +0000

    Bunch of changes to the IR demo code. - update to Python 3 and reindent (four-space indents) - add the query module so you can actually, y'know, do searches

--- trunk/spelman-talk/run_tests.py
+++ trunk/spelman-talk/run_tests.py
@@ -1,14 +1,14 @@
-#!/usr/bin/env python2.5
+#!/usr/bin/env python3
 
 from unittest import defaultTestLoader as loader, TextTestRunner as runner
 from sys import argv
 import tests
 
 if len(argv) > 1:
-  suite = loader.loadTestsFromNames(["tests.%s_tests" % s for s in argv[1:]])
+    suite = loader.loadTestsFromNames(["tests.%s_tests" % s for s in argv[1:]])
 else:
-  seq = ["tests.%s" % s for s in tests.__all__]
-  print seq
-  suite = loader.loadTestsFromNames(seq)
+    seq = ["tests.%s" % s for s in tests.__all__]
+    print(seq)
+    suite = loader.loadTestsFromNames(seq)
 
 runner().run(suite)
--- trunk/spelman-talk/tests/term_vector_tests.py
+++ trunk/spelman-talk/tests/term_vector_tests.py
@@ -6,9 +6,9 @@ words = "the quick brown fox jumps over the lazy sleeping dog".split()
 
 class TermVectorTests(TestCase):
 
-  def testCountWords(self):
-    counts = countwords(words)
+    def testCountWords(self):
+        counts = countwords(words)
 
-    assert counts['the'] == 2
-    assert counts['fox'] == 1
-    assert counts['mango'] == 0
+        assert counts['the'] == 2
+        assert counts['fox'] == 1
+        assert counts['mango'] == 0
