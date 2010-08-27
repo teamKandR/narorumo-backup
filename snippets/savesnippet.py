34895da3861e6d58c46a17b8d453c94b42d57648
alex.rudnick <alex.rudnick@8ef98eb1-aa40-0410-ae41-5dfa0a5cf510>
2010-08-27 02:46:09 +0000

    comment out example from the documentation

--- snippets/savesnippet.py
+++ snippets/savesnippet.py
@@ -5,22 +5,22 @@ def save(snippet, user):
     current time."""
     pass
 
-conn = sqlite3.connect('/tmp/example')
-c = conn.cursor()
-# Create table
-c.execute('''create table stocks
-(date text, trans text, symbol text,
- qty real, price real)''')
-
- # Insert a row of data
- c.execute("""insert into stocks
-           values ('2006-01-05','BUY','RHAT',100,35.14)""")
-
-# Save (commit) the changes
-conn.commit()
-
-# We can also close the cursor if we are done with it
-c.close()
+# conn = sqlite3.connect('/tmp/example')
+# c = conn.cursor()
+# # Create table
+# c.execute('''create table stocks
+# (date text, trans text, symbol text,
+#  qty real, price real)''')
+# 
+# # Insert a row of data
+# c.execute("""insert into stocks
+#        values ('2006-01-05','BUY','RHAT',100,35.14)""")
+# 
+# # Save (commit) the changes
+# conn.commit()
+# 
+# # We can also close the cursor if we are done with it
+# c.close()
 
 
 def main():
