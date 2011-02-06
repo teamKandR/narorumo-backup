697b7d85cd7f44ffce8b296c820abd69d7b5ea0b
alex.rudnick <alex.rudnick@8ef98eb1-aa40-0410-ae41-5dfa0a5cf510>
2011-02-06 02:10:25 +0000

    little cleanup on the shell script

--- mod_remixable/makeremixable.sh
+++ mod_remixable/makeremixable.sh
@@ -6,23 +6,18 @@
 ## Linuxes, such as for instance Ubuntu.
 
 # Convert from rtf to unicode.
-# pdftotext MachineofDeath_FINAL.pdf step1.txt
 killall -9 soffice.bin
 unoconv --format=text mod.rtf
 mv mod.txt step1.txt
 echo "OK converted rtf->unicode... "
 
-# Now unicode to flat ascii.
 iconv -f utf8 -t ascii//TRANSLIT step1.txt > step2.txt
 echo "OK converted unicode->ascii..."
-# Now fire up the Python script to take out the linefeeds, page numbers, and
-# just leave the stories that are CC-BY-NC-SA.
 
 echo "OK pulling out the remixable stories..."
 python3 ./justremixable.py step2.txt > mod_remixable.txt
 
 echo "OK cleaning up..."
-# Clean up the intermediate steps.
 rm -f step1.txt step2.txt
 
 echo "OK your remixable corpus should be in mod_remixable.txt. Share and enjoy!"
