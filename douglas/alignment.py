ea1b636fdc785165fb3aea440770cfdc0f3a8f99
alex.rudnick <alex.rudnick@8ef98eb1-aa40-0410-ae41-5dfa0a5cf510>
2011-05-14 01:15:35 +0000

    alignment.py can now learn the ibm model 1 translation tables, woo

--- douglas/alignment.py
+++ douglas/alignment.py
@@ -14,46 +14,73 @@ Input: set of sentence pairs (e,f)
 outout: translation prob t(e | f)
 """
 
+from collections import defaultdict
+from operator import itemgetter
+
 def translation_tables(esentences, fsentences):
     """Given two lists of sentences (each a list of words), build and
     return the translation table t(e|f), which is a dictionary of the shape
-    t[french][english] = prob."""
+    t[e][f] = prob."""
 
     t = {}
     evocab = set()
     fvocab = set()
 
+    # build e vocab
+    for sent in esentences:
+        for e in sent.split():
+            evocab.add(e)
+    print("got {0} english words.".format(len(evocab)))
+    # build f vocab
+    for sent in fsentences:
+        for f in sent.split():
+            fvocab.add(f)
+    print("got {0} foreign words.".format(len(fvocab)))
+
     # initialize t(e|f) uniformly
+    lowprob = 1 / len(evocab)
+    ewords = 0
     for eword in evocab:
-        for fword in fvocab:
-            t[fword] = {}
-            t[fword][eword] = 1 / len(evocab)
+        t[eword] = defaultdict(lambda:lowprob)
+        # for fword in fvocab:
+        #     t[eword][fword] = lowprob
+        if ewords % 1000 == 0: print(ewords,eword)
+        ewords += 1
 
     # while not converged...
     for step in range(5):
         print("step", step)
 
         # initialize
-        count(e|f) = 0 for all e, f
-        total(f) = 0 for all f
+        # count(e|f) = 0 for all e, f
+        count = defaultdict(lambda: 0)
 
-        for all sentence pairs(esent,fsent) do:
+        # total(f) = 0 for all f
+        total = defaultdict(lambda: 0)
+
+        s_total = {}
+        # for all sentence pairs(esent,fsent) do:
+        for (esent, fsent) in zip(esentences, fsentences):
             # compute normalization
-            for all words e in esent do:
-                s-total(e) = 0
-                for all words f in fsent do:
-                    s-total(e) += t(e|f)
+            for e in esent.split():
+                s_total[e] = 0
+                for f in fsent.split():
+                    s_total[e] += t[e][f]
 
             # collect counts
-            for all words e in esent do:
-                for all words f in fsent:
-                    count(e|f) += t(e|f) / s-total(e)
-                    total(f) += t(e|f) / s-total(e)
+            for e in esent.split():
+                for f in fsent.split():
+                    count[(e,f)] += t[e][f] / s_total[e]
+                    total[f] += t[e][f] / s_total[e]
 
         # estimate probabilities
-        for all foreign words f do:
-            for all English words e do:
-                t(e|f) = count(e|f) / total(f)
+        for f in fvocab:
+            for e in evocab:
+                if f in total:
+                    t[e][f] = count[(e,f)] / total[f]
+                else:
+                    t[e][f] = 0.0 ## need smoothing?
+    return t
 
 import os
 
@@ -61,12 +88,32 @@ englishpath = os.path.expanduser("~/corpora/es-en/europarl-v6.es-en.en")
 spanishpath = os.path.expanduser("~/corpora/es-en/europarl-v6.es-en.es")
 
 def getbitext(n):
+    esentences = []
+    fsentences = []
     with open (englishpath) as enfile:
-        with open (spanishpath) as esfile:
-            zipped = zip(enfile, esfile)
-            return [next(zipped) for i in range(n)]
+        for i in range(n):
+            esentences.append(enfile.readline())
+    with open (spanishpath) as esfile:
+        for i in range(n):
+            fsentences.append(esfile.readline())
+
+    return esentences, fsentences
+
+def repl(trans):
+    while True:
+        print("English word? ", end="")
+        e = input().strip()
+
+        if e in trans:
+            translations = trans[e].items()
+            inorder = sorted(translations, key=itemgetter(1), reverse=True)
+            print(inorder[:5])
+        else:
+            print("don't know {0}".format(e))
 
 def main():
-    print(getbitext(10))
+    esentences, fsentences = getbitext(500)
+    trans = translation_tables(esentences, fsentences)
+    repl(trans)
 
 if __name__ == "__main__": main()
