ec41378b6b956d895969d08fe39fd0fbeb3a49e7
lindseykuper@gmail.com <lindseykuper@gmail.com@8ef98eb1-aa40-0410-ae41-5dfa0a5cf510>
2011-06-17 23:21:39 +0000

    Added a bunch of infrastructure; removed dead code; less eliffy.

--- trunk/icfp11/src/dumbestpossible.py
+++ trunk/icfp11/src/dumbestpossible.py
@@ -2,17 +2,61 @@
 
 import sys
 
-def left_app(card, slot):
-    """Applies card to slot, leaving the result in slot."""
-    print("1")
-    print(card)
-    print(slot)
+turn_queue = []
 
-def right_app(slot, card):
-    """Applies slot to card, leaving the result in slot."""
-    print("2")
-    print(slot)
-    print(card)
+def run_all(tq):
+    while True:
+        try:
+            pop_and_print(tq)
+        except IndexError as e: # got 'em all
+            return
+
+def pop_and_print(tq):
+    move = tq.pop(0)
+    print(move[0])
+    print(move[1])
+    print(move[2])
+    
+def init_first_x_with_y(x, y):
+    """
+    Initializes the first X slots with the Yth power of 2, assuming
+    they already contain I.
+
+    Uses 2X + YX turns.  For instance initializing the first 3 slots
+    with the 2nd power of 2 (that is, 4) would require: zero, zero,
+    zero, one, one, one, dbl, dbl, dbl, dbl, dbl, dbl, or 2*3 + 2*3 ==
+    12 turns.
+    """
+
+    # Get 0s in.  Uses X turns.
+    for i in range(x):
+        enqueue_right_app(i, "zero")
+
+    # Get 1s in.  Uses X turns.
+    for i in range(x):
+        enqueue_left_app("succ", i)
+
+    # Get bigger numbers in.  Uses X turns for each doubling.
+    for i in range(x):
+        for j in range(y):
+            enqueue_left_app("dbl", i)
+
+    # Return the number of turns this takes.
+    return 2*x + y*x
+
+def enqueue_left_app(card, slot):
+    """
+    Enqueue an operation that applies card to slot, leaving the result
+    in slot.
+    """
+    turn_queue.append(["1", card, slot])
+
+def enqueue_right_app(slot, card):
+    """
+    Enqueue an operation that applies slot to card, leaving the result
+    in slot.
+    """
+    turn_queue.append(["2", slot, card])
 
 def get_opponent_move():
     option = input().strip()
@@ -28,65 +72,48 @@ def get_opponent_move():
 
 def playzero():
     """
-    Do a right application of our slot 0 to the zero card.  This
-    writes the value of ((lambda (x) x) 0), which is to say, 0, into
-    our slot 0.
+    Enqueue a right application of our slot 0 to the zero card.  When
+    run, this will write the value of ((lambda (x) x) 0), which is to
+    say, 0, into our slot 0.
     """
-    print("2")
-    print("0")
-    print("zero")
+    enqueue_right_app(0, "zero")
 
 def playdec(slot=0):
     """
-    Do a left application of the dec card to one of our slots (default
-    0).  Assuming that that slot has n in it, this side-effects the
-    opponent's (255-n)th slot, decrementing its vitality by 1, and
-    returns the identity function, causing our slot n to be
-    overwritten by the identity function.
+    Enqueue a left application of the dec card to one of our slots
+    (default 0).  Assuming that that slot has n in it, this will
+    side-effect the opponent's (255-n)th slot, decrementing its
+    vitality by 1, and return the identity function, causing our slot
+    n to be overwritten by the identity function.
     """
-    print("1")
-    print("dec")
-    print(slot)
+    enqueue_left_app("dec", slot)
 
 def playsucc(slot=0):
     """
-    Do a left application of the succ card to one of our slots
+    Enqueue a left application of the succ card to one of our slots
     (default 0).  Assuming the slot has n in it, the effect of
-    playsucc() is to write n+1 into the slot.  (Then we have an n+1 to
-    work with, as an argument to dec, for instance.)
+    playsucc() will be to write n+1 into the slot.  (Then we have an
+    n+1 to work with, as an argument to dec, for instance.)
     """
 
     # Constraints on slot size
     assert (slot >= 0)
     assert (slot <= 255)
 
-    print("1")
-    print("succ")
-    print(slot)
+    enqueue_left_app("succ", slot)
 
 def playdbl(slot=0):
     """
-    Do a left application of the dbl card to one of our slots
+    Enqueue a left application of the dbl card to one of our slots
     (default 0).  Assuming the slot has n in it, the effect of
-    playsucc() is to write n*2 into the slot.
+    playsucc() will be to write n*2 into the slot.
     """
 
     # Constraints on slot size
     assert (slot >= 0)
     assert (slot <= 255)
 
-    print("1")
-    print("dbl")
-    print(slot)
-
-def get_single_action(turns, action, current_turn):
-    # Say the current turn is 1, and we want to do 'action' for 8 turns.
-    # We'll be done when turn 9 is over.
-    stop_when = turns + current_turn
-
-    while (current_turn < stop_when):
-        yield action;
-        current_turn += 1;
+    enqueue_left_app("dbl", slot)
 
 commands = {
     "playzero" : playzero,
@@ -95,28 +122,21 @@ commands = {
     "playdbl" : playdbl,
 }
 
-def initialize_SK(slot, current_step):
-    """
-    Initializes the two slots starting at 'slot' with the S and K
-    combinators.  Threads the current step through.
-    """
-
-    # Right-app some identity slots to S and K cards.
-    right_app(slot, S)
-    right_app(slot, K)
-
-def strategy(cmds, turn):
+def enqueue_strategy(cmds, times):
     # On any given turn, we're only going to use one of the commands
     # in cmds.  Pick the right one out of the list based on which
     # of our turns it is.
+
+    """
+    Example: if there are 3 cmds and we want them run 10000 times,
+    then we have 30000 things to enqueue.
+    """
     splitted = cmds.split()
-    i = turn % len(splitted)
-
-    # Then pick the corresponding function out of the commands dict.
-    funk = commands[splitted[i]]
-
-    # And run it.
-    funk()
+    num_cmds = len(splitted)
+    for i in range(times * num_cmds):
+        # Pick the corresponding function out of the commands dict.
+        funk = commands[splitted[i % num_cmds]]
+        funk()
 
 def main():
     whoami = sys.argv[1]
@@ -132,38 +152,19 @@ def main():
         try:
             # Remember, turns in the actual game are numbered from 1,
             # not 0.
+            
+            # Write 16 into the first 5 slots.  Side-effects the turn
+            # queue.
+            init_first_x_with_y(5, 4)
 
-            if turn <= 1:
-                # Set up slot 0 with 1.
-                strategy("playzero playsucc", turn)
+            # Enqueue 10,000 alternations of each strategy.  (We won't
+            # get to all of these.)
+            enqueue_strategy("playzero playdec", 10000)
+            enqueue_strategy("playzero playsucc playdec", 10000)
+            enqueue_strategy("playzero playsucc playsucc playdec", 10000)
 
-            elif turn <= 7:
-                # Do playdbl 6 times on slot 0.
-                gen = get_single_action(6, playdbl, turn)
-                f = next(gen)
-                f()
-
-                # The above is total overkill (could've just written
-                # playdbl() for the same effect), but I'm hoping that
-                # eventually we'll be able to use the generator to
-                # return the appropriate step of a sophisticated
-                # multi-action sequence...
-
-            elif turn == 8:
-                # Decrement opponent's slot number (255-64) by one.
-                playdec()
-
-            else:
-
-                if turn < 20000:
-                    # Decrement opponent's slot 255.
-                    strategy("playzero playdec", turn)
-                elif turn <= 50000:
-                    # Decrement opponent's slot 254.
-                    strategy("playzero playsucc playdec", turn)
-                else:
-                    # Decrement opponent's slot 253.
-                    strategy("playzero playsucc playsucc playdec", turn)
+            # Run 'em all.
+            run_all(turn_queue)
 
             option, card, slot = get_opponent_move()
             turn += 1
