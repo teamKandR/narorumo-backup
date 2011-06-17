4fe0625b3d90c2db62218d9ef666b7bc956aca81
lindseykuper@gmail.com <lindseykuper@gmail.com@8ef98eb1-aa40-0410-ae41-5dfa0a5cf510>
2011-06-17 06:28:59 +0000

    Docstring'd.

--- icfp11/src/dumbestpossible.py
+++ icfp11/src/dumbestpossible.py
@@ -15,11 +15,23 @@ def get_opponent_move():
     return option, card, slot
 
 def playzero():
+    """
+    Do a right application of our slot 0 to the zero card.  This
+    writes the value of ((lambda (x) x) 0), which is to say, 0, into
+    our slot 0.
+    """
     print("2")
     print("0")
     print("zero")
 
 def playdec():
+    """
+    Do a left application of the dec card to our slot 0.  This
+    side-effects the opponent's (255-0)th slot (that is, their 255th
+    slot), decrementing its vitality by 1, and returns the identity
+    function (causing our slot 0 to be overwritten by the identity
+    function again).
+    """
     print("1")
     print("dec")
     print("0")
