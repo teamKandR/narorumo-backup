7cd8ec41a79169b5a20ea19a2eebe0f3b0b2ac97
alex.rudnick <alex.rudnick@8ef98eb1-aa40-0410-ae41-5dfa0a5cf510>
2011-06-18 07:00:06 +0000

    now with functions to construct fanny and greg too

--- icfp11/src/yfireball.py
+++ icfp11/src/yfireball.py
@@ -7,16 +7,25 @@ from ltg_util import pop_and_print
 from ltg_util import get_opponent_move
 from ltg_util import apply_card
 from ltg_util import apply_slot
+from ltg_util import apply_slot0_to_slot1
 
 from ycombinator import build_y_combinator
 
 def build_fanny():
-    # TODO...
-    pass
+    """((S (K K)) (K K)).
+    Uses slots 0 and 1, result ends up in slot 0."""
+    apply_card("put", 1)
+    apply_slot(1, "K")
+    apply_card("K", 1)
+    build_greg(0)
+    apply_slot0_to_slot1()
 
-def build_greg():
-    # TODO...
-    pass
+def build_greg(slot):
+    """(S (K K))"""
+    apply_card("put", slot)
+    apply_slot(slot, "K")
+    apply_card("K", slot)
+    apply_card("S", slot)
 
 def build_horace(slot):
     """(S (K S))"""
@@ -30,20 +39,22 @@ def build_ian(slot):
     apply_card("put", slot)
     apply_card("K", slot)
 
- # We want this:
- # ((S
- #   ((S (K S))
- #    ((S
- #      ((S (K S))
- #       ((S ((S (K S)) ((S (K K)) (K S))))
- #        ((S ((S (K S)) ((S (K K)) (K K)))) ((S (K K)) I)))))
- #     ((S ((S (K S)) ((S (K K)) (K K)))) (K I)))))
- #  ((S ((S (K S)) ((S (K K)) (K dec)))) (K I)))
+# We want this:
+# ((S
+#   (horace
+#    ((S
+#      (horace
+#       ((S (horace (greg (K S))))
+#        ((S (horace fanny)) (greg I)))))
+#     ((S (horace fanny)) ian))))
+#  ((S (horace (greg (K dec)))) ian))
 
 def main():
-    build_y_combinator()
-    build_horace(1)
-    build_ian(2)
+    # build_y_combinator()
+    build_fanny()
+    build_greg(11)
+    build_horace(12)
+    build_ian(13)
     apply_card("ffff", 0)
     gameloop()
 
