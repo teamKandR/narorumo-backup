bf26bbd55cffebaee1e6352c40f5c47365916be2
alex.rudnick <alex.rudnick@8ef98eb1-aa40-0410-ae41-5dfa0a5cf510>
2011-06-18 06:32:28 +0000

    functions to build horace and ian

--- icfp11/src/yfireball.py
+++ icfp11/src/yfireball.py
@@ -6,14 +6,29 @@ from ltg_util import enqueue_strategy
 from ltg_util import pop_and_print
 from ltg_util import get_opponent_move
 from ltg_util import apply_card
+from ltg_util import apply_slot
 
 from ycombinator import build_y_combinator
 
 def build_fanny():
     # TODO...
+    pass
 
 def build_greg():
     # TODO...
+    pass
+
+def build_horace(slot):
+    """(S (K S))"""
+    apply_card("put", slot)
+    apply_slot(slot, "S")
+    apply_card("K", slot)
+    apply_card("S", slot)
+
+def build_ian(slot):
+    """(K I)"""
+    apply_card("put", slot)
+    apply_card("K", slot)
 
  # We want this:
  # ((S
@@ -27,6 +42,8 @@ def build_greg():
 
 def main():
     build_y_combinator()
+    build_horace(1)
+    build_ian(2)
     apply_card("ffff", 0)
     gameloop()
 
